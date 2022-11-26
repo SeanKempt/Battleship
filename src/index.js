93937ff8fe1ab199a2d73eb395195269fa82177a
SeanKempt <kempt101@gmail.com>
2022-11-26 11:06:58 -0600

    feat: add random attack generator for player factory

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import { gameBoard } from './modules/gameBoard.js';
 import { ship } from './modules/ship.js';
 import { Player } from './modules/player.js';
 
+const testComputerPlayer = Player('computerPlayer', true);
 const destroyer = ship(4, 'destroyer');
 const skif = ship(2, 'skif');
 const submarine = ship(2, 'submarine');
@@ -11,3 +12,5 @@ game.placeShip([1, 3], [2, 3], skif);
 console.log(destroyer.getHits());
 game.receiveAttack([0, 3], [3, 0]);
 console.log(destroyer.getHits());
+testComputerPlayer.randomAttack(game);
+console.log(game.getMissedAttacks());
--- src/modules/player.js
+++ src/modules/player.js
@@ -1,15 +1,32 @@
 //might need to look into creating the gameboard on the players themselves
 
-const Player = (name) => {
+const Player = (name, isComputer = false) => {
   const changeTurn = () =>
     playerTurn === true ? (playerTurn = false) : (playerTurn = true);
   const attack = (x, y, board) => {
     board.receiveAttack(x, y);
     changeTurn();
   };
+  const getIsComputer = () => isComputer;
   let playerTurn = true;
   const isTurn = () => playerTurn;
-  return { name, isTurn, changeTurn, attack };
+
+  const randomAttack = (gameBoard) => {
+    const _randNum = () => {
+      return Math.floor(Math.random() * 10) + 1;
+    };
+    if (getIsComputer() === true && isTurn() === true) {
+      attack([_randNum(), _randNum()], [_randNum(), _randNum()], gameBoard);
+    }
+  };
+  return {
+    name,
+    isTurn,
+    changeTurn,
+    attack,
+    getIsComputer,
+    randomAttack,
+  };
 };
 
 export { Player };
