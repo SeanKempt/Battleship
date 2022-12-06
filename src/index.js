38001ca58cfd0fd321639ee130ee87a832b169c7
SeanKempt <kempt101@gmail.com>
2022-12-06 11:48:49 -0600

    feat: add shipSetup function to place ships on board

--- src/index.js
+++ src/index.js
@@ -1,24 +1,4 @@
-import { GameBoard } from './modules/gameBoard.js';
-import { Ship } from './modules/ship.js';
-import { Player } from './modules/player.js';
+import './style.css';
 import { gameSetup } from './modules/gameLogic.js';
 
-import './style.css';
-import {
-  renderComputerGameBoard,
-  renderPlayerGameBoard,
-} from './modules/domChanges.js';
-const testGameBoard = GameBoard();
-const computerTestGameBoard = GameBoard();
-const testPlayer = Player('Ezsean');
-const testComputerPlayer = Player('testComputer', true);
-testGameBoard.placeShip('carrier', [
-  [0, 0],
-  [0, 1],
-  [0, 2],
-  [0, 3],
-  [0, 4],
-]);
-
-renderPlayerGameBoard(testGameBoard);
-renderComputerGameBoard(computerTestGameBoard, testPlayer);
+gameSetup();
