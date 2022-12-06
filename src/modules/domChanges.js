b06acda5b325f09aa0b240c1e5196a25225e2911
SeanKempt <kempt101@gmail.com>
2022-12-06 11:11:10 -0600

    bug: fix rendering of squares for computer and player

--- src/modules/domChanges.js
+++ src/modules/domChanges.js
@@ -1,13 +1,33 @@
 const playerBoard = document.getElementById('player-board');
 const computerBoard = document.getElementById('computer-board');
 
-const renderPlayerGameBoard = (gameBoardSquares) => {
-  for (let i = 0; i < gameBoardSquares.length; i++) {
-    const square = document.createElement('div');
-    square.dataset.cord = gameBoardSquares[i];
-    square.innerHTML = '';
-    square.classList.add('gameSquare');
-    playerBoard.appendChild(square);
+const renderPlayerGameBoard = (board) => {
+  const boardSquares = board.getBoard();
+  for (let i = 0; i < boardSquares.length; i++) {
+    for (let j = 0; j < boardSquares[i].length; j++) {
+      const square = document.createElement('div');
+      square.dataset.cord = [[i], [j]];
+      square.innerHTML = '';
+      square.classList.add('gameSquare');
+      playerBoard.appendChild(square);
+    }
+  }
+};
+
+//player obj is the player that is created when the game starts. Not the computer. computer would be computerObj
+//cpuBoard is the computers gameBoard. since that is the only one that is receiving attacks from the player clicking on the board.
+const renderComputerGameBoard = (cpuBoard, playerObj) => {
+  const cpuSquares = cpuBoard.getBoard();
+  for (let i = 0; i < cpuSquares.length; i++) {
+    for (let j = 0; j < cpuSquares[i].length; j++) {
+      const square = document.createElement('div');
+      square.dataset.cord = [[i], [j]];
+      let cord = square.dataset.cord;
+      square.innerHTML = '';
+      square.classList.add('gameSquare');
+      attackEventListener(square, playerObj, cpuBoard, cord);
+      computerBoard.appendChild(square);
+    }
   }
 };
 
@@ -19,20 +39,9 @@ const attackEventListener = (element, playerObj, enemyBoard, cord) => {
     newCord.push(parseInt(strings[i]));
   }
   element.addEventListener('click', () => {
-    playerObj.attack(newCord, enemyBoard);
+    const [x, y] = newCord;
+    playerObj.attack(x, y, enemyBoard);
   });
-};
-
-const renderComputerGameBoard = (gameBoardSquares, playerObj, enemyBoard) => {
-  for (let i = 0; i < gameBoardSquares.length; i++) {
-    const square = document.createElement('div');
-    square.dataset.cord = gameBoardSquares[i];
-    let cord = square.dataset.cord;
-    square.innerHTML = '';
-    square.classList.add('gameSquare');
-    attackEventListener(square, playerObj, enemyBoard, cord);
-    computerBoard.appendChild(square);
-  }
 };
 
 export { renderPlayerGameBoard, renderComputerGameBoard };
