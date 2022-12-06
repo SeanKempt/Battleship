43d8a0121ec49e7f32ece1aab1d8fad367bc8018
SeanKempt <kempt101@gmail.com>
2022-12-06 11:11:59 -0600

    cosmetic: clean up console log entries

--- src/index.js
+++ src/index.js
@@ -4,7 +4,14 @@ import { Player } from './modules/player.js';
 import { gameSetup } from './modules/gameLogic.js';
 
 import './style.css';
+import {
+  renderComputerGameBoard,
+  renderPlayerGameBoard,
+} from './modules/domChanges.js';
 const testGameBoard = GameBoard();
+const computerTestGameBoard = GameBoard();
+const testPlayer = Player('Ezsean');
+const testComputerPlayer = Player('testComputer', true);
 testGameBoard.placeShip('carrier', [
   [0, 0],
   [0, 1],
@@ -12,6 +19,6 @@ testGameBoard.placeShip('carrier', [
   [0, 3],
   [0, 4],
 ]);
-console.log(testGameBoard.getBoard());
-console.log(testGameBoard.receiveAttack(0, 0));
-console.log(testGameBoard.getPastAttacks());
+
+renderPlayerGameBoard(testGameBoard);
+renderComputerGameBoard(computerTestGameBoard, testPlayer);
